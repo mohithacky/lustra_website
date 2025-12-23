7b59c1cec1b6e5b1294d779bcc977f59727179d9
abhisoniabhi <166976750+abhisoniabhi@users.noreply.github.com>
2025-12-23 16:55:01 +0530

    changed something more and more and more and more and more  and more

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1243585778"
+    serviceWorkerVersion: "2354297069"
   }
 });
